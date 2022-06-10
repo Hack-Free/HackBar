85b1de75d55e0f4b92316512d37ded2988cbdcb2
Chewbaka <3535011+Chewbaka69@users.noreply.github.com>
2022-06-10 14:23:39 +0200

    Fixe error HOST from other tab when post

--- hackbar/background.js
+++ hackbar/background.js
@@ -2,56 +2,67 @@
  When we receive the message, execute the given script in the given
  tab.
  */
-'use strict';
-let currentPostData = [];
-let headers = []
-let curentHeaders = [];
+ 'use strict';
+ let currentPostData = [];
+ let headers = []
+ let curentHeaders = [];
+ 
+ // get POST data for each loaded page
+ browser.webRequest.onBeforeRequest.addListener(
+     setCurrentPostData, {
+         urls: ['<all_urls>'],
+         types: ['main_frame']
+     },
+     ['requestBody']
+ );
+ 
+ 
+ // add/modify headers before sending request
+ browser.webRequest.onBeforeSendHeaders.addListener(
+     rewriteHeaders, {
+         urls: ["<all_urls>"],
+         types: ["main_frame"]
+     },
+     ["blocking", "requestHeaders"]
+ );
+ 
+ 
+ // get headers for each page loaded
+ browser.webRequest.onSendHeaders.addListener(
+     getCurrentHeaders, {
+         urls: ["<all_urls>"],
+         types: ["main_frame"]
+     },
+     ["requestHeaders"]
+ );
+ 
+ function setCurrentPostData(e) {
+     if (e.method === "POST" && e.requestBody) {
+         let rawData = e.requestBody.formData;
+         var post_data_array = [];
+         for (let key in rawData) {
+             if (rawData.hasOwnProperty(key)) {
+                 var item = key + "=" + rawData[key];
+                 post_data_array.push(item);
+             }
+         }
+ 
+         browser.tabs.query({
+                 currentWindow: true,
+                 active: true
+             },
+             function(tabArray) {
+                 const currentTabId = tabArray[0].id;
+                 currentPostData[currentTabId] = post_data_array.join("&");
+             }
+         );
+     }
+ }
+ 
+ async function rewriteHeaders(e) {
+     let tab = await browser.tabs.query({ currentWindow: true, active: true })
+     let currentTabId = tab[0].id;
 
-// get POST data for each loaded page
-browser.webRequest.onBeforeRequest.addListener(
-    setCurrentPostData,
-    { urls: ['<all_urls>'], types: ['main_frame'] }, 
-    ['requestBody']
-);
-
-
-// add/modify headers before sending request
-browser.webRequest.onBeforeSendHeaders.addListener(
-    rewriteHeaders,
-    { urls: ["<all_urls>"], types: ["main_frame"] },
-    ["blocking", "requestHeaders"]
-);
-
-
-// get referer for each page loaded
-browser.webRequest.onSendHeaders.addListener(
-    getCurrentHeaders,
-    { urls: ["<all_urls>"], types: ["main_frame"] },
-    ["requestHeaders"]
-);
-
-function setCurrentPostData(e) {
-    if (e.method === "POST" && e.requestBody) {
-        let rawData = e.requestBody.formData;
-        var post_data_array = [];
-        for (let key in rawData) {
-            if (rawData.hasOwnProperty(key)) {
-                var item = key + "=" + rawData[key];
-                post_data_array.push(item);
-            }
-        }
-
-        browser.tabs.query(
-            {currentWindow: true, active: true},
-            function (tabArray) {
-                const currentTabId = tabArray[0].id;
-                currentPostData[currentTabId] = post_data_array.join("&");
-            }
-        );
-    }
-}
-
-function rewriteHeaders(e) {
     let index_referer, index_user_agent, index_cookie;
     index_cookie = index_referer = index_user_agent = -1;
     for (let i = 0; i < e.requestHeaders.length; i++) {
@@ -68,101 +79,116 @@ function rewriteHeaders(e) {
                 break;
         }
     }
+
     //add referer
-    if (headers.referer) {
+    if (headers[currentTabId].referer) {
         if (index_referer !== -1) {
-            e.requestHeaders[index_referer].value = headers.referer;
+            e.requestHeaders[index_referer].value = headers[currentTabId].referer;
         } else {
             e.requestHeaders.push({
                 name: "Referer",
-                value: headers.referer
+                value: headers[currentTabId].referer
             });
         }
     }
+
     //modify user agent
-    if (headers.user_agent) {
+    if (headers[currentTabId].user_agent) {
         if (index_user_agent !== -1) {
-            e.requestHeaders[index_user_agent].value = headers.user_agent;
+            e.requestHeaders[index_user_agent].value = headers[currentTabId].user_agent;
         } else {
             e.requestHeaders.push({
                 name: "User-Agent",
-                value: headers.user_agent
+                value: headers[currentTabId].user_agent
             });
         }
     }
+
     //modify cookie
-    if (headers.cookie) {
+    if (headers[currentTabId].cookie) {
         if (index_cookie !== -1) {
-            e.requestHeaders[index_cookie].value = headers.cookie;
+            e.requestHeaders[index_cookie].value = headers[currentTabId].cookie;
         } else {
             e.requestHeaders.push({
                 name: "Cookie",
-                value: headers.cookie
+                value: headers[currentTabId].cookie
             });
         }
     }
+    
     // custom header
-    if (headers.custom) {
-        for(let header in headers.custom) {
+    if (headers[currentTabId].custom) {
+        for (let header in headers[currentTabId].custom) {
             let found = e.requestHeaders.findIndex(element => element.name === header);
 
-            if(found !== -1)
-                e.requestHeaders[found].value = headers.custom[header];
+            if (found !== -1)
+                e.requestHeaders[found].value = headers[currentTabId].custom[header];
             else
                 e.requestHeaders.push({
                     name: header,
-                    value: headers.custom[header]
+                    value: headers[currentTabId].custom[header]
                 });
         }
     }
-    return {requestHeaders: e.requestHeaders};
-}
 
-function getCurrentHeaders( e ) {
-    browser.tabs.query(
-        {currentWindow: true, active: true},
-        function (tabArray) {
-            const currentTabId = tabArray[0].id;
-            let _headers = [];
-            _headers.custom = [];
-            for( let h of e.requestHeaders ) {
-                if( h.name.toLowerCase() === 'referer' ) {
-                    _headers.referer = h.value;
-                } else if( h.name.toLowerCase() === 'user-agent' ) {
-                    _headers.user_agent = h.value;
-                } else if( h.name.toLowerCase() === 'cookie' ) {
-                    _headers.cookie = h.value;
-                } else {
-                    _headers.custom[h.name] = h.value;
-                }
-            }
-            curentHeaders[currentTabId] = _headers;
-        }
-    );
-
-}
-
-function handleMessage(request, sender, sendResponse) {
-    if (sender.url !== browser.runtime.getURL("/theme/hackbar-panel.html")) {
-        return;
-    }
-    const tabId = request.tabId;
-    const action = request.action;
-    switch (action) {
-        case 'load_url':
-            browser.tabs.get(tabId, function (tab) {
-                sendResponse({url: tab.url, data: currentPostData[tabId], headers: curentHeaders[tabId]});
-            });
-            break;
-        case 'send_requests':
-            headers = request.data.headers;
-            sendResponse({status: true});
-            break;
-    }
-    return true;
-}
-
-/**
- Listen for messages from our devtools panel.
- */
-browser.runtime.onMessage.addListener(handleMessage);
+    return { requestHeaders: e.requestHeaders };
+ }
+ 
+ function getCurrentHeaders(e) {
+     browser.tabs.query({
+             currentWindow: true,
+             active: true
+         },
+         function(tabArray) {
+             const currentTabId = tabArray[0].id;
+             let _headers = [];
+             _headers.custom = [];
+             for (let h of e.requestHeaders) {
+                 if (h.name.toLowerCase() === 'referer') {
+                     _headers.referer = h.value;
+                 } else if (h.name.toLowerCase() === 'user-agent') {
+                     _headers.user_agent = h.value;
+                 } else if (h.name.toLowerCase() === 'cookie') {
+                     _headers.cookie = h.value;
+                 } else {
+                     _headers.custom[h.name] = h.value;
+                 }
+             }
+             curentHeaders[currentTabId] = _headers;
+         }
+     );
+ 
+ }
+ 
+ function handleMessage(request, sender, sendResponse) {
+     if (sender.url !== browser.runtime.getURL("/theme/hackbar-panel.html")) {
+         return;
+     }
+     const tabId = request.tabId;
+     const action = request.action;
+     switch (action) {
+         case 'load_url':
+             browser.tabs.get(tabId, function(tab) {
+                 sendResponse({
+                     url: tab.url,
+                     data: currentPostData[tabId],
+                     headers: curentHeaders[tabId]
+                 });
+             });
+             break;
+         case 'send_requests':
+             browser.tabs.get(tabId, function(tab) {
+                 headers[tabId] = request.data.headers;
+                 sendResponse({
+                     status: true
+                 });
+             });
+             break;
+     }
+     return true;
+ }
+ 
+ /**
+  Listen for messages from our devtools panel.
+  */
+ browser.runtime.onMessage.addListener(handleMessage);
